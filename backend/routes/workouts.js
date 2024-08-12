6f7bb1acad45a4e24281a3a6b7ed9bc111715ed9
Imen Saghrouni <saghrouniimen@tbs.u-tunis.tn>
2024-08-12 13:07:58 +0100

    first commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { 
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout,
+} = require('../controllers/workoutController')
+
+
+const router = express.Router()
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+
+
+//GET single workout
+router.get('/:title', getWorkout)
+
+//POST new workout
+router.post('/',createWorkout)
+
+//DELETE new workout
+router.delete('/:id',deleteWorkout)
+
+//UPDATE new workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const wourkoutRoutes = require('./routes/workouts')
+
+//express app
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.use('/api/workouts',wourkoutRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //listen for requests(port)
+        app.listen(process.env.PORT, () =>  {
+            console.log('connected to db and listening on port 4000')
+        })
+
+})
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
+
